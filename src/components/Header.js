1681c2e987bad7c32b0530cabb90aebd670d058e
shiv5768 <sshivlal9601@gmail.com>
2023-12-30 17:18:55 +0530

    Changed the Header site layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,21 @@
 import React from "react";
 import '../style/Header.css'
+import logo from '../images/[PY’PRO].png'
 
 const Header = () => {
     return (
-        <div>
+        <div className="nav-header">
             <head>
                 <title>PYPRO - Community for pros</title>
                 <link rel="stylesheet" type="text/css" href="styles.css" />
             </head>
             <body>
                 <header>
-                    <h1 className="com-name">PYPRO</h1>
+                    <img src={logo} alt="logo" className="logo"/>
                     <nav>
                         <ul>
                             <li><a href="#">Home</a></li>
                             <li><a href="#">Events</a></li>
-                            <li><a href="#">Projects</a></li>
                             <li><a href="#">Resources</a></li>
                             <li><a href="#">Questions</a></li>
                         </ul>
