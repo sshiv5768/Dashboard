b2dac986aee2be4fc199815726211fec7bd3e250
sshiv5768 <sshivlal9601@gmail.com>
2024-01-27 14:07:37 +0530

    Added test text file

--- src/components/Eventlist.js
+++ src/components/Eventlist.js
@@ -1,28 +1,28 @@
 import EventCard from "./Eventcard";
-import htf from "../images/2-2.png"
-import aws from "../images/AWS_CommunityDay_Ahmedabad.png"
+import React, {useState, useEffect} from "react";
+
 export default function EventList() {
+   
+    const [eventData, setEventData] = useState([ ]);
 
-    const eventData = [
-        {
-            eventName: "Hack This Fall 2024",
-            eventDescription: "Dive into innovation at Hack This Fall Hackathon, where collaboration meets creativity! Join us in cultivating a beginner-friendly space for diverse minds to unite, ideate, and bring meaningful projects to life – experience the true essence of a hackathon journey.",
-            eventImage: 'https://dashboard-event-pics.s3.ap-south-1.amazonaws.com/2-2.png',
-            eventLink: 'https://hackthisfall.tech/'
-        },
-        {
-          eventName: "AWS Community Day Ahmedabad 2024",
-          eventDescription: "Embark on a cloud journey at AWS Community Day Ahmedabad! Join us live on January 21st for a day of insights, networking, and cutting-edge discussions in the heart of AWS innovation.",
-          eventImage: 'https://dashboard-event-pics.s3.ap-south-1.amazonaws.com/AWS_CommunityDay_Ahmedabad.png',
-          eventLink: 'https://awsahmedabad.community/'
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('/src/components/events.json');
+          const data = await response.json();
+          setEventData(data);
         }
-
-    ]
+        catch (error){
+          console.error('Error fetching data!', error);
+        }
+      }
+      fetchData();
+    }, []);
 
   return (
     <div className="flex justify-center flex-wrap overflow-x-auto overflow-y-auto p-4 ">
     {eventData.map((event, index) => (
-      <div key={index} className="mx-2 my-2">
+      <div className="mx-2 my-2">
         <EventCard key={index} {...event}/> 
       </div>  
     ))}
